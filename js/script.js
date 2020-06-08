8a2c4fb443b928ecde5ed75a93f49a1fb40928c4
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-08 19:52:46 -0400

    Added functionality to questions JavaScript

--- js/script.js
+++ js/script.js
@@ -16,19 +16,36 @@ fetch(url).then(function(res){
   game.arr = data.data;
   data.data.forEach(function(el){
     console.log(el);
-  })
+  });
   createQuestion();
 })
-
-
-
-
 
 
 
 function createQuestion(){
   nx.style.display = "none";
 
+  message.textContent = 'Question #'+(game.val+1)+ ' out of '+ game.total;
+  output.innerHTML = '';
+  console.log(game);
+  let q = game.arr[game.val];
+  console.log(q);
+  const main = document.createElement('div');
+  main.textContent = q.question;
 
+  output.appendChild(main);
 
+  q.opt.forEach(function(el){
+    console.log(el);
+    let span = document.createElement('span');
+    span.textContent = el;
+    output.appendChild(span);
+    span.ans = q.answer;
+    span.addEventListener('click', checker);
+  })
+}
+
+function checker(e){
+  console.log(e.target.ans);
+  console.log(this.ans);
 }
