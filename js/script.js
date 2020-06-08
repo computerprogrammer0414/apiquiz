5ce326daac5a0d7608588d19a9912df79c79a4bd
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-08 19:16:32 -0400

    Added Initial Files; html, JS and CSS

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const message = document.querySelector('.message');
+const game = {};
+const output = document.querySelector('.que');
+const nx = document.querySelector('.next');
+
+nx.addEventListener('click', createQuestion);
+
+function createQuestion(){
+  nx.style.display = "none";
+
+
+
+}
