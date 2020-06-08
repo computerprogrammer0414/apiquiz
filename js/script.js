54c6c601eb550aaa29cdc5c18a8fb64ed6e312ab
Jaime C Smith <computerprogrammer0414@gmail.com>
2020-06-08 19:37:41 -0400

    Added API connectivity to Google Spreadsheet

--- js/script.js
+++ js/script.js
@@ -4,6 +4,27 @@ const output = document.querySelector('.que');
 const nx = document.querySelector('.next');
 
 nx.addEventListener('click', createQuestion);
+const url = 'https://script.google.com/macros/s/AKfycbyaXSH1Dsrd_JVNfSPWblP0vcwjcPCE3Y5Mgq9zbGUUVkW5_6Af/exec';
+
+fetch(url).then(function(res){
+  return res.json()
+}).then(function(data){
+  console.log(data);
+  game.total = data.data.length;
+  game.val = 0;
+  game.score = 0;
+  game.arr = data.data;
+  data.data.forEach(function(el){
+    console.log(el);
+  })
+  createQuestion();
+})
+
+
+
+
+
+
 
 function createQuestion(){
   nx.style.display = "none";
